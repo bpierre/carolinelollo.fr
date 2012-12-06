6c3f8d04df6a37a12a39969be706c6c55ab5f67f
Pierre Bertet <bonjour@pierrebertet.net>
2012-12-06 01:18:39 +0100

    HTML fixes

--- footer.php
+++ footer.php
@@ -1,13 +1,10 @@
 	</div><!--.container-->
-	<div id="footer"><footer>
-		</div>
-	</footer></div><!--#footer-->
 </div><!--#main-->
-<script type='text/javascript' src='https://ajax.googleapis.com/ajax/libs/jquery/1.6.1/jquery.min.js'></script>
-<script type='text/javascript' src='<?php bloginfo('template_url'); ?>/s/jquery.scrollTo-min.js'></script>
-<script type='text/javascript' src='<?php bloginfo('template_url'); ?>/s/main.js'></script>
+<script src='http://ajax.googleapis.com/ajax/libs/jquery/1.8/jquery.min.js'></script>
+<script src='<?php bloginfo('template_url'); ?>/s/jquery.scrollTo-min.js'></script>
+<script src='<?php bloginfo('template_url'); ?>/s/main.js'></script>
 <?php wp_footer(); /* this is used by many Wordpress features and for plugins to work proporly */ ?>
-<script type="text/javascript">
+<script>
 
   var _gaq = _gaq || [];
   _gaq.push(['_setAccount', 'UA-2658463-1']);
@@ -18,7 +15,6 @@
     ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
     var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
   })();
-
 </script>
 </body>
 </html>
