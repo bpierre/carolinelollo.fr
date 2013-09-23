7b33aaace73e7515ddcf7932ec7342e931936161
Pierre Bertet <bonjour@pierrebertet.net>
2013-09-23 09:22:31 +0100

    HTML structure corrections

--- app/views/home.html.php
+++ app/views/home.html.php
@@ -16,7 +16,7 @@
 <div id="gkLogoContainer">
 	<div id="gkLogo">
 		<a href="http://gifsykings.carolinelollo.fr/">
-			<img src="http://27.media.tumblr.com/tumblr_llimoxB9K21qzgwljo1_400.gif"/>
+			<img src="http://27.media.tumblr.com/tumblr_llimoxB9K21qzgwljo1_400.gif" alt="">
 			<p>Gifsy Kings</p>
 			<div id="warped">
 				<span class="w0">G</span><span class="w1">I</span><span class="w2">F</span><span class="w3">S</span><span class="w4">Y</span><span class="w5">K</span><span class="w6">I</span><span class="w7">N</span><span class="w8">G</span><span class="w9">S</span>
@@ -25,8 +25,28 @@
 	</div>
 </div>
 <div id="side">
-	<a href="http://gifsykings.carolinelollo.fr/"></a>
 	<div id="gkPosts">
-		<h2></h2><div id="tumblr_recent_photos"><ul><a href="http://gifsykings.carolinelollo.fr/post/26016170596" target="_blank" ><img src="http://25.media.tumblr.com/tumblr_m6aiw3Zfiz1qzgwljo1_100.gif" border="0" alt="http://gifsykings.carolinelollo.fr/post/26016170596" style="" /></a><a href="http://gifsykings.carolinelollo.fr/post/26016147341" target="_blank" ><img src="http://25.media.tumblr.com/tumblr_m6aivgMVQg1qzgwljo1_100.gif" border="0" alt="http://gifsykings.carolinelollo.fr/post/26016147341" style="" /></a><a href="http://gifsykings.carolinelollo.fr/post/26016118422" target="_blank" ><img src="http://25.media.tumblr.com/tumblr_m6aiuouXZL1qzgwljo1_100.gif" border="0" alt="http://gifsykings.carolinelollo.fr/post/26016118422" style="" /></a></ul></div><br clear="all" /> <p class="more"><a href="http://gifsykings.carolinelollo.fr/" title="Go to Gifsy Kings">+</a></p>
+		<div id="tumblr_recent_photos">
+			<ul>
+				<li>
+					<a href="http://gifsykings.carolinelollo.fr/post/26016170596" target="_blank">
+						<img src="http://25.media.tumblr.com/tumblr_m6aiw3Zfiz1qzgwljo1_100.gif" alt="">
+					</a>
+				</li>
+				<li>
+					<a href="http://gifsykings.carolinelollo.fr/post/26016147341" target="_blank">
+						<img src="http://25.media.tumblr.com/tumblr_m6aivgMVQg1qzgwljo1_100.gif" alt="">
+					</a>
+				</li>
+				<li>
+					<a href="http://gifsykings.carolinelollo.fr/post/26016118422" target="_blank">
+						<img src="http://25.media.tumblr.com/tumblr_m6aiuouXZL1qzgwljo1_100.gif" alt="">
+					</a>
+				</li>
+			</ul>
+		</div>
+		<p class="more">
+			<a href="http://gifsykings.carolinelollo.fr/" title="Go to Gifsy Kings">+</a>
+		</p>
 	</div>
 </div>
